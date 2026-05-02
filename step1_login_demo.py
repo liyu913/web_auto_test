414a15f794d60d7cc0a00500500e53265fa1db88
li Yu <287509645@qq.com>
2026-05-02 15:47:27 +0800

    测试

--- step1_login_demo.py
+++ step1_login_demo.py
@@ -1,4 +1,20 @@
-def print_hi(name):
-    print(f'Hi, {name}!')
+# step1_login_demo.py
+import requests
 
-print_hi('PyCharm')
+url = "http://192.168.168.64:48080/admin-api/system/auth/login"
+
+payload = {
+    "username": "0366",
+    "password": "1111D"
+}
+
+headers = {
+    "Content-Type": "application/json",
+    "tenant-id": "1"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.status_code)
+print(response.text)
+print(response.json())
